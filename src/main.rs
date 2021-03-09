4eb5457d0ef1a692d31196cba81c5100d27df90f
Thomas Hennes <jaxxeh@gmail.com>
2021-03-09 17:33:08 +0100

    CI update + added tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+    // We're removing the hardcoded `8000` value for the port,
+    // it's now coming from our settings
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
